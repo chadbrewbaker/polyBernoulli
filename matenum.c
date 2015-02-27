028cae753f22f914eea9b1aa8e06e7b0536eb931
Chad Brewbaker <crb002@gmail.com>
2015-02-27 16:50:45 -0600

    Added checks for all 2x2 matrix subsets

--- matenum.c
+++ matenum.c
@@ -2,10 +2,52 @@
 
 int M[15][15];
 
-int PATTERN[2][2] = {{1, 0}, {0, 1}};
-unsigned long long int COUNT;
+/*#ruby
+vals = [0,1]
+vals = vals.repeated_permutation(2).to_a
+mats = vals.repeated_permutation(2).to_a.inspect.to_s
+puts mats.gsub(/]/ , '}').gsub(/\[/ , '{')
+*/
+int PATTERN[16][2][2] = {{{0, 0}, {0, 0}},
+                         {{0, 0}, {0, 1}},
+                         {{0, 0}, {1, 0}},
+                         {{0, 0}, {1, 1}},
+                         {{0, 1}, {0, 0}},
+                         {{0, 1}, {0, 1}},
+                         {{0, 1}, {1, 0}},
+                         {{0, 1}, {1, 1}},
+                         {{1, 0}, {0, 0}},
+                         {{1, 0}, {0, 1}},
+                         {{1, 0}, {1, 0}},
+                         {{1, 0}, {1, 1}},
+                         {{1, 1}, {0, 0}},
+                         {{1, 1}, {0, 1}},
+                         {{1, 1}, {1, 0}},
+                         {{1, 1}, {1, 1}}};
+#define PRINT_MASKS_TOGGLE 0
 
+int PATTERN_MASK;
+#define CHECK_BIT(var,pos) ((var) & ( 1 << (pos)))
+
+unsigned long long int COUNT;
 int MAX = 5;
+
+void printPatternMask() {
+  int i;
+  for (i = 0; i < 16; i++) {
+    printf("%d", !!CHECK_BIT(PATTERN_MASK,i) ); 
+  }
+  printf("\n");
+}
+
+int nextSubset() {
+  PATTERN_MASK++;
+  /*Quit when we overflow*/
+  if ( PATTERN_MASK == 65536) 
+    return 0;
+  else
+    return 1;
+}
 
 void printM(int n, int k) {
   int i, j;
@@ -18,35 +60,35 @@ void printM(int n, int k) {
   printf("\n");
 }
 
-int avoidsPartialPattern(int n, int k, int n2, int k2) {
+int avoidsPartialPatterns(int n, int k, int n2, int k2) {
   int i, j;
   int hit;
+  int mask;
   if ((k2 == 0) || (n2 == 0))
     return 1;
   for (i = 0; i < n2; i++) {
+
     for (j = 0; j < k2; j++) {
-      hit = 0;
-      hit += (M[i][j] == PATTERN[0][0]);
-      hit += (M[i][k2] == PATTERN[0][1]);
-      hit += (M[n2][j] == PATTERN[1][0]);
-      hit += (M[n2][k2] == PATTERN[1][1]);
-      if (hit == 4)
-        return 0;
-      hit = 0;
-      hit += (M[i][j] == !PATTERN[0][0]);
-      hit += (M[i][k2] == !PATTERN[0][1]);
-      hit += (M[n2][j] == !PATTERN[1][0]);
-      hit += (M[n2][k2] == !PATTERN[1][1]);
-      if (hit == 4)
-        return 0;
+
+      mask = 0;
+      for (mask = 0; mask < 16; mask++) {
+        if (CHECK_BIT(PATTERN_MASK,mask)) {
+          hit = 0;
+          hit += (M[i][j] == PATTERN[mask][0][0]);
+          hit += (M[i][k2] == PATTERN[mask][0][1]);
+          hit += (M[n2][j] == PATTERN[mask][1][0]);
+          hit += (M[n2][k2] == PATTERN[mask][1][1]);
+          if (hit == 4)
+            return 0;
+        }
+      }
     }
   }
-
   return 1;
 }
 
 void dfs_sub(int n, int k, int n2, int k2) {
-  if (!avoidsPartialPattern(n, k, n2, k2))
+  if (!avoidsPartialPatterns(n, k, n2, k2))
     return;
   if (k2 < (k - 1)) {
     k2++;
@@ -75,15 +117,22 @@ void dfs(int n, int k) {
   dfs_sub(n, k, 0, 0);
   M[0][0] = 1;
   dfs_sub(n, k, 0, 0);
-  printf("%llu ZeroOne matrices of size %d %d \n ", COUNT, n, k);
+  printf("%llu %d %d ", COUNT, n, k);
 }
 
 int main() {
   int i, j;
-  for (i = 1; i < MAX; i++) {
-    for (j = 1; j < MAX; j++) {
-      dfs(i, j);
+  PATTERN_MASK=0; 
+ while (nextSubset()) {
+    for (i = 1; i < MAX; i++) {
+      for (j = 1; j < MAX; j++) {
+        dfs(i, j);
+      }
     }
+     if(PRINT_MASKS_TOGGLE)
+     	printPatternMask();
+     else
+        printf("\n");
   }
   return 0;
 }
